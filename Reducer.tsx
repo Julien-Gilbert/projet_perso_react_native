61c404ad2249416f60519c85ae73a116f4223e76
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-09 09:35:13 -0400

    Initialisation of the reducer

---
+++ Reducer.tsx
@@ -0,0 +1,11 @@
+
+const initialState = { favoritesBook: [] }
+
+export default function toggleFavorite(state = initialState, action) {
+    let nextState;
+
+    switch(action.type) {
+        default:
+            return state
+    }
+}
