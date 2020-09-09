e8a87bd9afd9d12d7c6d7575b79acd162218a05e
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-09 16:03:18 -0400

    Display books in the favorite page

--- Favorites.tsx
+++ Favorites.tsx
@@ -1,10 +1,29 @@
 import React, { Component } from "react"
 import { StyleSheet, Text, TextInput, View, StatusBar, FlatList } from 'react-native'
+import DisplayBook from "./DisplayBook"
+import { connect } from 'react-redux'
 
 export default class favorites extends Component {
     render() {
         return(
-            <Text>Liste des livres favoris</Text>
+            <FlatList
+                data={this.props.favoritesBook}
+                renderItem={({item}) => <DisplayBook book={item}/>}
+            />
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        favoritesBook: state.favoritesBook
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatch: (action) => { dispatch(action) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(favorites)
