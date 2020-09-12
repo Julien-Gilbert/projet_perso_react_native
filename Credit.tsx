174ac0d17549802ba9553595f648a04d67b64ea9
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-12 15:54:57 -0400

    Scroll a simple text

--- Credit.tsx
+++ Credit.tsx
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Animated, Text, Button, View, ScrollView } from "react-native";
 
 export class Credit extends Component {
+
+    marginTopInit = -20
+
+    state = {
+        scrollAnim: new Animated.Value(this.marginTopInit)
+    } 
+
+    launchScrollCredit = () => {
+        Animated.timing(this.state.scrollAnim, {
+            toValue: 1000,
+            duration: 10000,
+            useNativeDriver: false
+        }).start()
+    }
+
     render() {
         return (
-            <Text>Credits</Text>
+            <View style={{flex: 1}}>
+            <ScrollView>
+                <Animated.View style={[{
+                    marginTop: this.state.scrollAnim
+                }]}>
+                    <Text>Test</Text>
+                </Animated.View>
+            </ScrollView>
+               
+                <Button title="Test" onPress={this.launchScrollCredit} />
+                <Button title="Reset" onPress={Animated.timing(this.state.scrollAnim).reset} />
+            </View>
         )
     }
 }
+
