7a2a7db44f4262b17f185dd8c38f4799f1154582
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-12 14:00:55 -0400

    Implementation of FlipCard

--- Animation.tsx
+++ Animation.tsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Animated, View, Text, Button, StyleSheet } from "react-native";
+import FlipCard from 'react-native-flip-card'
 
 export class Animation extends Component {
+    
     render() {
         return (
-            <Text>Animation</Text>
+            <FlipCard 
+                //style={styles.card}
+                friction={6}
+                perspective={1000}
+                flipHorizontal={true}
+                flipVertical={false}
+                flip={false}
+                clickable={true}
+                onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
+            >
+                {/* Face Side */}
+                <View>
+                    <Text>The Face</Text>
+                </View>
+                
+                {/* Back Side */}
+                <View>
+                    <Text>The Back</Text>
+                </View>
+            </FlipCard>
         )
     }
 }
