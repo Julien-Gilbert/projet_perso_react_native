0c60ee8687440c666838c413141a30ac726c1109
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-12 14:17:25 -0400

    Add of the credit page

---
+++ Credit.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export class Credit extends Component {
+    render() {
+        return (
+            <Text>Credits</Text>
+        )
+    }
+}
