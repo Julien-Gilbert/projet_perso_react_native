a882faa4ec942d7375ebd3abbcdede52b4104f7b
Julien <julien.gilbert@phelma.grenoble-inp.fr>
2020-09-14 21:19:45 -0400

    Flipcard with Harry Potter book

--- Animation.tsx
+++ Animation.tsx
@@ -1,31 +1,47 @@
 import React, { Component } from "react";
-import { Animated, View, Text, Button, StyleSheet } from "react-native";
+import { Animated, View, Text, Button, StyleSheet, Image } from "react-native";
 import FlipCard from 'react-native-flip-card'
 
 export class Animation extends Component {
-    
     render() {
         return (
+                
             <FlipCard 
-                //style={styles.card}
+                style={styles.flipCard}
                 friction={6}
                 perspective={1000}
                 flipHorizontal={true}
                 flipVertical={false}
                 flip={false}
                 clickable={true}
-                onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
             >
-                {/* Face Side */}
-                <View>
-                    <Text>The Face</Text>
-                </View>
                 
-                {/* Back Side */}
-                <View>
-                    <Text>The Back</Text>
-                </View>
+                    <Image
+                        style={styles.card}
+                        source={require('./Images/page_couverture.png')}
+                    />
+               
+
+                
+                    <Image
+                        style={styles.card}
+                        source={require('./Images/4eme_page.png')}
+                    />
+                
             </FlipCard>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    flipCard: {
+        flex:1,
+        alignItems:'center'
+    },
+    card: {
+        marginTop: 30,
+        width: 300,
+        height: 450,
+        resizeMode: 'stretch'
+    }
+})
